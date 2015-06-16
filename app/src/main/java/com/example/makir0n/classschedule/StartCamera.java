48af2ed66ea232218642ad39f98b09898df5a860
Makir0n <trp19946@gmail.com>
2015-06-16 23:51:12 +0900

    Camera intent

--- app/src/main/java/com/example/makir0n/classschedule/StartCamera.java
+++ app/src/main/java/com/example/makir0n/classschedule/StartCamera.java
@@ -1,44 +1,21 @@
 package com.example.makir0n.classschedule;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Camera;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class Scedule extends ActionBarActivity {
+public class StartCamera extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scedule);
+        setContentView(R.layout.activity_start_camera);
 
-        final Button EditBtn = (Button) findViewById(R.id.EditBtn);
-        EditBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startSubActivity();
-            }
-        });
-
-        final Button CameraBtn = (Button) findViewById(R.id.CameraBtn);
-        CameraBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startCameraActivity();
-            }
-        });
-    }
-    private void startSubActivity(){
-        Intent intent=new Intent(this,Edit.class);
-        startActivityForResult(intent,0);
-
-    }
-
-    private void startCameraActivity(){
         Intent intent = new Intent();
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
@@ -49,14 +26,14 @@ public class Scedule extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scedule, menu);
+        getMenuInflater().inflate(R.menu.menu_start_camera, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up edit, so long
+        // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
